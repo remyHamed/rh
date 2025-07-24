ff7cbce421191e63be561a633c7a9d45f5942493
hemarydem <hamedremy@hotmail.fr>
2025-07-24 13:01:59 +0200

    feat: scale animation ok

--- src/presentation/components/Header/Header.tsx
+++ src/presentation/components/Header/Header.tsx
@@ -1,29 +1,39 @@
+import { useState } from "react";
 import SlidingLetter from "../SlidingLetter/SlidingLetter"
 
 function Header() {
+
+    const [isFullscreen, setIsFullscreen] = useState(false);
+
+    function handleClick() {
+        setIsFullscreen(!isFullscreen);
+    }
+
+
     return (
-        <header
-        className="
-            group
-            mt-4
-            w-[10vw]
-            h-[10vw]
-            bg-black
-            transition-all
-            duration-300
-            hover:scale-110
-            origin-top-left
-            flex
-            items-center
-            justify-center
-            gap-[0.2vw]
-            overflow-hidden
-        "
+        <header 
+        onClick={handleClick}
+        className={`
+                group
+                mt-4
+                bg-black
+                transition-all
+                duration-500
+                origin-top-left
+                flex
+                items-center
+                justify-center
+                gap-[0.2vw]
+                overflow-hidden
+                ${isFullscreen ? "fixed top-0 left-0 w-screen h-screen z-50" : "w-[10vw] h-[10vw]"}
+            `}
         >
-            <SlidingLetter letter="M" />
-            <SlidingLetter letter="E" />
-            <SlidingLetter letter="N" />
-            <SlidingLetter letter="U" />
+            <div className="absolute mt-[4vw] ml-[1vw] top-0 left-0 flex gap-[0.2vw]">
+                <SlidingLetter letter="M" />
+                <SlidingLetter letter="E" />
+                <SlidingLetter letter="N" />
+                <SlidingLetter letter="U" />
+            </div>
         </header>
     );
 }

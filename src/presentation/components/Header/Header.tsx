0dbbdb9951801ef8e3e9a68b9a0531f090e240d4
hemarydem <hamedremy@hotmail.fr>
2025-07-24 13:53:53 +0200

    feat : header ok

--- src/presentation/components/Header/Header.tsx
+++ src/presentation/components/Header/Header.tsx
@@ -12,8 +12,8 @@ function Header() {
 
     return (
         <header 
-        onClick={handleClick}
-        className={`
+            onClick={handleClick}
+            className={`
                 group
                 mt-4
                 bg-black
@@ -26,14 +26,30 @@ function Header() {
                 gap-[0.2vw]
                 overflow-hidden
                 ${isFullscreen ? "fixed top-0 left-0 w-screen h-screen z-50" : "w-[10vw] h-[10vw]"}
-            `}
+                `}
         >
-            <div className="absolute mt-[4vw] ml-[1vw] top-0 left-0 flex gap-[0.2vw]">
-                <SlidingLetter letter="M" />
-                <SlidingLetter letter="E" />
-                <SlidingLetter letter="N" />
-                <SlidingLetter letter="U" />
-            </div>
+        <div 
+            className="
+                absolute 
+                mt-[4vw] 
+                ml-[1vw] 
+                top-0 
+                left-0 flex gap-[0.2vw]
+            "
+        >
+            <SlidingLetter letter="M" />
+            <SlidingLetter letter="E" />
+            <SlidingLetter letter="N" />
+            <SlidingLetter letter="U" />
+        </div>
+            {isFullscreen && (
+                <nav className="absolute flex items-center justify-center gap-4 p-2">
+                <a href="/" className="text-white">Accueil</a>
+                <a href="/diplomes" className="text-white">Diplômes</a>
+                <a href="/experience" className="text-white">Expérience</a>
+                <a href="/projets" className="text-white">Projets</a>
+                </nav>
+            )}
         </header>
     );
 }

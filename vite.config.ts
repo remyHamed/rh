8a9bae241e54ef877a8c89d96ede26eb712bfbb8
hemarydem <hamedremy@hotmail.fr>
2025-07-28 11:15:53 +0200

    wip: update git config

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/rh/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 });
